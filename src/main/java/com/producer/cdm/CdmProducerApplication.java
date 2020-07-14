1743db8eb266a2c58bbcd2b4fe61b1d9148c0d0e
d36choi <yuio1126@gmail.com>
2020-07-14 19:07:02 +0900

    init

---
+++ src/main/java/com/producer/cdm/CdmProducerApplication.java
@@ -0,0 +1,13 @@
+package com.producer.cdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdmProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdmProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/producer/cdm/CdmProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producer.cdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdmProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
