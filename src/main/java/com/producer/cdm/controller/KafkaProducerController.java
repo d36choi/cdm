1fd9cec24d613ff4041a7c35e7ece133d72a97a4
d36choi <40600306+d36choi@users.noreply.github.com>
2020-11-22 14:27:24 +0900

    하나의 json data 를 바로 kafka 에 넣도록 수정함 return value 를 ResponseEntity 객체로 수정함

--- src/main/java/com/producer/cdm/controller/KafkaProducerController.java
+++ src/main/java/com/producer/cdm/controller/KafkaProducerController.java
@@ -4,14 +4,15 @@ package com.producer.cdm.controller;
 import com.producer.cdm.model.UberData;
 import com.producer.cdm.repository.UberJpaRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.kafka.core.KafkaTemplate;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Controller
@@ -20,21 +21,31 @@ public class KafkaProducerController {
     @Autowired
     KafkaTemplate<String,Object> kafkaTemplate;
 
+    HashSet<String> topicList = new HashSet<>(Arrays.asList("car_spec","user_info",
+            "user_license","reservation","otp","test"));
 
     @PostMapping("/publish")
     @ResponseBody
-    public HashMap<String, Object> post(@RequestParam String topic, @RequestBody Map<String,Object> map)
+    public ResponseEntity<HashMap<String, Object>> post(@RequestParam String topic, @RequestBody Map<String,Object> map)
     {
         HashMap<String,Object> res = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        for( Map.Entry<String, Object> elem : map.entrySet() ){
-            sb.append(elem.getKey());
-            log.info("send... "+elem);
-            kafkaTemplate.send(topic, elem);
+
+        if (!topicList.contains(topic)) {
+            res.put("error","wrong topic name");
+            log.error("wrong topic name taken");
+            return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
         }
+        log.info("send... "+map);
+        kafkaTemplate.send(topic,map);
+//        for( Map.Entry<String, Object> elem : map.entrySet() ){
+//            sb.append(elem.getKey());
+//            log.info("send... "+elem);
+//            kafkaTemplate.send(topic, elem);
+//        }
         res.put("topic",topic);
         res.put("Success",map);
 
-        return res;
+        return new ResponseEntity<>(res,HttpStatus.OK);
     }
+
 }
